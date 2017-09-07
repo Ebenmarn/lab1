a5f727d8b98903484258fe7c8cd69c29521dfa1e
Eben Marneweck <eben.d.marneweck@gmail.com>
2017-09-07 16:27:55 -0400

    Made it compute area

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -4,20 +4,24 @@ public class Rectangle {
 
 	private double Length;
 	private double Width;
+
 	public Rectangle(double length, double width) {
 		Length = length;
 		Width = width;
 	}
+
 	public double getLength() {
 		return Length;
 	}
+
 	public double getWidth() {
 		return Width;
 	}
-	
-	public double Area()
-	{
-		// TODO Implement Area() function with the correct formula
-		return 0;
-	}	
+
+	public double Area() {
+		double area = getLength() * getWidth();
+		// THIS MAKES AREA
+
+		return area;
+	}
 }
